dbad988fcb20fae8b2965c050767954ba44aa273
杨杰鸿 <yangjiehong@yangjiehongdeMacBook-Air.local>
2026-03-26 13:54:35 +0800

    Change currency to USD

--- garment-customizer/src/utils/priceCalculator.js
+++ garment-customizer/src/utils/priceCalculator.js
@@ -105,9 +105,9 @@ export function calculateQuote(state) {
 }
 
 export function formatPrice(price) {
-  return new Intl.NumberFormat('zh-CN', {
+  return new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'CNY',
+    currency: 'USD',
     minimumFractionDigits: 2,
   }).format(price);
 }
